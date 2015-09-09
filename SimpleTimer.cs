956e1eacbca74f6a74dbcbdb05cf53175c4fcae1
Edison Scott <edison@foxcubgames.com>
2015-09-09 11:25:49 -0700

    -Committing Engine

---
+++ SimpleTimer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class SimpleTimer : MonoBehaviour {
+
+	public Text _timerText;
+	public Text _startStopButtonLabel;
+
+	private float _timer = 0.0f;
+	private bool _isRunning = false;
+
+	private void Start () {
+		this._timer = 0.0f;
+		this._isRunning = false;
+		this._timerText.text = "0";
+	}
+
+	private void Update () {
+		if (this._isRunning) {
+			this._timer += Time.deltaTime;
+			this._timerText.text = this._timer.ToString();
+		}
+	}
+
+	public void ToggleStartStop(){
+		this._isRunning = !this._isRunning;
+
+		if (this._isRunning) {
+			this._startStopButtonLabel.text = "Stop";
+		} else {
+			this._startStopButtonLabel.text = "Start";
+		}
+	}
+
+	public void Reset(){
+		this._timer = 0;
+		this._timerText.text = "0";
+	}
+}
